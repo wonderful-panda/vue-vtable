3ed93eb011f7c061dc2638b589060aa0c8abc7ae
Iwata Hidetaka <iwata0303@gmail.com>
2018-05-25 05:58:04 +0900

    modify slightly

--- src/propopts.ts
+++ src/propopts.ts
@@ -5,18 +5,23 @@ import * as t from "../types";
 export function getPropOptions<T>() {
     // prettier-ignore
     return {
+        // for all
+        rowHeight: p(Number).validator(v => v > 0).required,
+        getItemKey: p.ofFunction<t.GetKeyFunction<T>>().required,
+        rowStyleCycle: p(Number).validator(v => v > 0).default(1),
+        // for Vlist
         contentWidth: p(Number, String).optional,
+        // for Vlist / VtableBase
         itemCount: p(Number).required,
         sliceItems: p.ofFunction<t.SliceFunction<T>>().required,
-        getItemKey: p.ofFunction<t.GetKeyFunction<T>>().required,
-        rowHeight: p(Number).validator(v => v > 0).required,
-        rowStyleCycle: p(Number).validator(v => v > 0).default(1),
+        // for VtableBase / Vtable
         headerHeight: p(Number).validator(v => v >= 0).optional,
         columns: p.ofRoArray<t.VtableColumn>().required,
-        items: p.ofRoArray<T>().required,
         splitterWidth: p(Number).validator(v => v > 0).default(3),
         rowClass: p(String).optional,
         getRowClass: p.ofFunction<t.GetClassFunction<T>>().default(() => (() => undefined)),
-        initialWidths: p.ofRoArray<number>().optional
+        initialWidths: p.ofRoArray<number>().optional,
+        // for Vtable
+        items: p.ofRoArray<T>().required
     };
 }
