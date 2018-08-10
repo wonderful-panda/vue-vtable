cfb5f1a171355319dcc30bf6e7e08891f05658ff
Iwata Hidetaka <iwata0303@gmail.com>
2018-08-10 12:09:19 +0900

    remove build output

--- dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function px(value: number | string | undefined): string | undefined;
-export declare function supplier<T>(value: T): () => T;
-export declare function ensureNotUndefined<T>(value: T | undefined): T;
