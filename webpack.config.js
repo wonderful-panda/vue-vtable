0669531347d41f9a4dc2d173c0ee45ab8c417343
Iwata Hidetaka <iwata0303@gmail.com>
2019-09-14 11:19:04 +0900

    upgrade webpack and babel
    Now storybook does not work

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 var path = require("path");
 
+var mode = process.env.NODE_ENV;
+
 module.exports = {
-    context: path.join(__dirname, "src"),
-    entry: "./index.ts",
-    output: {
-        library: "vue-vlist",
-        libraryTarget: "umd",
-        path: path.join(__dirname, "dist"),
-        filename: "index.js"
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        modules: [ path.join(__dirname, "src"), "node_modules" ]
-    },
-    module: {
-        loaders: [
-          { test: /\.tsx?$/, loader: "babel-loader!ts-loader", exclude: /node_modules/ }
-        ]
-    },
-    externals: [
-        "vue",
-        "vue-tsx-support",
-        "vue-typed-component",
-        "lodash"
+  context: path.join(__dirname, "src"),
+  entry: "./index.ts",
+  mode,
+  output: {
+    library: "vue-vlist",
+    libraryTarget: "umd",
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    modules: [path.join(__dirname, "src"), "node_modules"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader!ts-loader",
+        exclude: /node_modules/
+      }
     ]
+  },
+  externals: ["vue", "vue-tsx-support", "lodash"]
 };
